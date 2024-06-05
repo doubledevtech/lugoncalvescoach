ccddbe01890e853c12ff822a522c6ce39a0e5626
Flávio Lima <doubledevit@gmail.com>
2024-06-05 10:16:41 -0300

    Fazer deploy da amostra para desktop.

---
+++ slide.js
@@ -0,0 +1,16 @@
+let radio = document.querySelector('.manual__btn');
+let count = 1;
+
+document.getElementById('radio1').checked = true;
+
+setInterval(() => {
+    nextImg(); 
+}, 5000 );
+
+function nextImg() {
+    count ++;
+    if (count > 4) {
+        count = 1;
+    }
+    document.getElementById('radio'+ count).checked = true;
+}
